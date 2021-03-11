02997a82eefa6175903c87a6b524e666941fc3d4
Tyler Wray <wraytw@gmail.com>
2021-03-11 15:16:21 -0700

    Move categories response struct

--- lib/plaid/categories.ex
+++ lib/plaid/categories.ex
@@ -3,6 +3,35 @@ defmodule Plaid.Categories do
   [Plaid Categories API](https://plaid.com/docs/api/products/#categoriesget) calls and schema.
   """
 
+  defmodule GetResponse do
+    @moduledoc """
+    [Plaid /categories/get API response schema.](https://plaid.com/docs/api/products/#categoriesget).
+    """
+
+    @behaviour Plaid.Castable
+
+    alias Plaid.Castable
+    alias Plaid.Categories.Category
+
+    @type t :: %__MODULE__{
+            categories: [Category.t()],
+            request_id: String.t()
+          }
+
+    defstruct [
+      :categories,
+      :request_id
+    ]
+
+    @impl true
+    def cast(generic_map) do
+      %__MODULE__{
+        categories: Castable.cast_list(Category, generic_map["categories"]),
+        request_id: generic_map["request_id"]
+      }
+    end
+  end
+
   @doc """
   Get information about all Plaid categories.
 
@@ -13,16 +42,15 @@ defmodule Plaid.Categories do
   ## Example
 
       get()
-      {:ok, %Plaid.Categories.GetResponse{}}
+      {:ok, %GetResponse{}}
 
   """
-  @spec get(Plaid.noauth_config()) ::
-          {:ok, Plaid.Categories.GetResponse.t()} | {:error, Plaid.Error.t()}
+  @spec get(Plaid.noauth_config()) :: {:ok, GetResponse.t()} | {:error, Plaid.Error.t()}
   def get(config \\ []) do
     Plaid.Client.call(
       "/categories/get",
       %{},
-      Plaid.Categories.GetResponse,
+      GetResponse,
       config
     )
   end
