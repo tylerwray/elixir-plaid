f7427c76594938bb9c2e6aa01e9b180b481205cd
Tyler Wray <wraytw@gmail.com>
2021-03-11 22:50:52 -0700

    v0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plaid.MixProject do
   def project do
     [
       app: :elixir_plaid,
-      version: "0.2.0",
+      version: "0.2.1",
       description: description(),
       package: package(),
       elixir: "~> 1.10",
