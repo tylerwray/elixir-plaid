e19f4c0fb4a2d40ecc6c5e22e77cb32ca4f3faec
Kevin Tayah <ktayah@yahoo.com>
2022-03-15 19:02:48 -0400

    Fixed issue with plaid not accepting nil value

--- lib/plaid/link_token/user.ex
+++ lib/plaid/link_token/user.ex
@@ -5,13 +5,13 @@ defmodule Plaid.LinkToken.User do
 
   @type t :: %__MODULE__{
           client_user_id: String.t(),
-          legal_name: String.t() | nil,
-          phone_number: String.t() | nil,
-          phone_number_verified_time: String.t() | nil,
-          email_address: String.t() | nil,
-          email_address_verified_time: String.t() | nil,
-          ssn: String.t() | nil,
-          date_of_birth: String.t() | nil
+          legal_name: String.t() | none(),
+          phone_number: String.t() | none(),
+          phone_number_verified_time: String.t() | none(),
+          email_address: String.t() | none(),
+          email_address_verified_time: String.t() | none(),
+          ssn: String.t() | none(),
+          date_of_birth: String.t() | none()
         }
 
   @enforce_keys [:client_user_id]
