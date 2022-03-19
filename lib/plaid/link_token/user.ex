796cea16cae18ec809b0c3a1c0c152a69f6ca080
Kevin Tayah <32495802+ktayah@users.noreply.github.com>
2022-03-19 17:49:57 -0400

    Adds optional fields to link token user
    Co-authored-by: Tyler Wray <wraytw@gmail.com>

--- lib/plaid/link_token/user.ex
+++ lib/plaid/link_token/user.ex
@@ -5,13 +5,13 @@ defmodule Plaid.LinkToken.User do
 
   @type t :: %__MODULE__{
           client_user_id: String.t(),
-          legal_name: String.t(),
-          phone_number: String.t(),
-          phone_number_verified_time: String.t(),
-          email_address: String.t(),
-          email_address_verified_time: String.t(),
-          ssn: String.t(),
-          date_of_birth: String.t()
+          optional(:legal_name) => String.t(),
+          optional(:phone_number) => String.t(),
+          optional(:phone_number_verified_time) => String.t(),
+          optional(:email_address) => String.t(),
+          optional(:email_address_verified_time) => String.t(),
+          optional(:ssn) => String.t(),
+          optional(:date_of_birth) => String.t()
         }
 
   @enforce_keys [:client_user_id]
