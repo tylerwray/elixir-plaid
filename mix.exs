d89e2cb6646439caefaf13e9d3b7bcac694678af
Tyler Wray <wraytw@gmail.com>
2023-08-01 20:15:07 -0600

    Version bump v1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plaid.MixProject do
   def project do
     [
       app: :elixir_plaid,
-      version: "1.2.0",
+      version: "1.2.1",
       description: description(),
       package: package(),
       elixir: "~> 1.10",
